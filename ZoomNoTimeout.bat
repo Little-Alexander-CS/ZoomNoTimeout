683c28826af751a3ac990f55be7f2b9e40cedce1
Little-Alexander-CS <77220782+Little-Alexander-CS@users.noreply.github.com>
2023-05-05 19:04:29 -0400

    Update ZoomNoTimeout.bat

--- ZoomNoTimeout.bat
+++ ZoomNoTimeout.bat
@@ -6,7 +6,7 @@ echo Unmuting Zoom...
 echo WScript.CreateObject("WScript.Shell").SendKeys("+1") > %temp%\ShiftPlus1.vbs && cscript //nologo %temp%\ShiftPlus1.vbs && del %temp%\ShiftPlus1.vbs
 
 rem Wait for 1 second
-ping -n 2 127.0.0.1 > nul
+ping -n 1 127.0.0.1 > nul
 
 echo Muting Zoom...
 echo WScript.CreateObject("WScript.Shell").SendKeys("+1") > %temp%\ShiftPlus1.vbs && cscript //nologo %temp%\ShiftPlus1.vbs && del %temp%\ShiftPlus1.vbs
@@ -15,5 +15,5 @@ rem Wait for 1 second
 ping -n 2 127.0.0.1 > nul
 
 rem Wait for x seconds before looping (can be changed)
-timeout /t 10 > nul
+timeout /t 30 > nul
 goto loop

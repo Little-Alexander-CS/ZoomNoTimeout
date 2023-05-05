f0c8692e0d43b3839b80c8d73519cb13d009b397
Little-Alexander-CS <77220782+Little-Alexander-CS@users.noreply.github.com>
2023-05-05 18:38:11 -0400

    Add files via upload

---
+++ ZoomNoTimeoutW.bat
@@ -0,0 +1,21 @@
+@echo off
+echo "SHIFT+1 must be set as the keyboard shortcut in Settings -> Keyboard shortcuts -> Mute/unmute my audio also tick Enable Global Shortcut"
+:loop
+
+echo Simulating SHIFT+1...
+echo WScript.CreateObject("WScript.Shell").SendKeys("+1") > %temp%\ShiftPlus1.vbs && cscript //nologo %temp%\ShiftPlus1.vbs && del %temp%\ShiftPlus1.vbs
+
+rem Wait for 1 second
+ping -n 2 127.0.0.1 > nul
+
+echo Simulating SHIFT+1 again...
+
+rem Simulate Alt+A key combination again
+echo WScript.CreateObject("WScript.Shell").SendKeys("+1") > %temp%\ShiftPlus1.vbs && cscript //nologo %temp%\ShiftPlus1.vbs && del %temp%\ShiftPlus1.vbs
+
+rem Wait for 1 second
+ping -n 2 127.0.0.1 > nul
+
+rem Wait for 60 seconds before looping
+timeout /t 10 > nul
+goto loop

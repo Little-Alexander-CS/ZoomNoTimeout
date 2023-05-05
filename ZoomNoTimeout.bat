ae839075c6c2b77776d315b811f771eb81a21772
Little-Alexander-CS <77220782+Little-Alexander-CS@users.noreply.github.com>
2023-05-05 18:44:18 -0400

    Update ZoomNoTimeout.bat

--- ZoomNoTimeout.bat
+++ ZoomNoTimeout.bat
@@ -1,19 +1,19 @@
 @echo off
-echo "SHIFT+1 must be set as the keyboard shortcut in Settings -> Keyboard shortcuts -> Mute/unmute my audio also tick Enable Global Shortcut"
+echo "SHIFT+1 must be set as the keyboard shortcut in Zoom: Settings -> Keyboard shortcuts -> Mute/unmute my audio also tick Enable Global Shortcut"
 
 :loop
-echo Simulating SHIFT+1...
+echo Unmuting Zoom...
 echo WScript.CreateObject("WScript.Shell").SendKeys("+1") > %temp%\ShiftPlus1.vbs && cscript //nologo %temp%\ShiftPlus1.vbs && del %temp%\ShiftPlus1.vbs
 
 rem Wait for 1 second
 ping -n 2 127.0.0.1 > nul
 
-echo Simulating SHIFT+1 again...
+echo Muting Zoom...
 echo WScript.CreateObject("WScript.Shell").SendKeys("+1") > %temp%\ShiftPlus1.vbs && cscript //nologo %temp%\ShiftPlus1.vbs && del %temp%\ShiftPlus1.vbs
 
 rem Wait for 1 second
 ping -n 2 127.0.0.1 > nul
 
-rem Wait for 10 seconds before looping
+rem Wait for x seconds before looping (can be changed)
 timeout /t 10 > nul
 goto loop

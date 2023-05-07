8244e5acdbca47eda269ea78106e21801506f83f
Little-Alexander-CS <77220782+Little-Alexander-CS@users.noreply.github.com>
2023-05-07 14:14:40 -0400

    Update ZoomNoTimeout.bat

--- ZoomNoTimeout.bat
+++ ZoomNoTimeout.bat
@@ -2,21 +2,22 @@
 title ZoomNoTimeout (Alexander Little)
 echo "ALT+1 must be set as the keyboard shortcut in Zoom:" 
 echo "Settings -> Keyboard shortcuts -> Mute/unmute my audio also tick Enable Global Shortcut"
-set /a x=0
+
+set /a counter=0
+set /a pause=30
+set /a pauseInBetween=1
+
 :loop
-echo Unmuting Zoom... %x%
+echo Unmuting Zoom... %counter%
 echo WScript.CreateObject("WScript.Shell").SendKeys("%%1") > %temp%\AltPlus1.vbs && cscript //nologo %temp%\AltPlus1.vbs && del %temp%\AltPlus1.vbs
 
-rem Wait for ~x-1 seconds (can be changed)
-ping -n 1 127.0.0.1 > nul
+rem Wait for pauseInBetween-1 seconds (can be changed)
+ping -n %pauseInBetween% 127.0.0.1 > nul
 
-echo Muting Zoom... %x%
+echo Muting Zoom... %counter%
 echo WScript.CreateObject("WScript.Shell").SendKeys("%%1") > %temp%\AltPlus1.vbs && cscript //nologo %temp%\AltPlus1.vbs && del %temp%\AltPlus1.vbs
 
-rem Wait for 1 second
-ping -n 2 127.0.0.1 > nul
-
-rem Wait for x seconds before looping (can be changed)
-timeout /t 30 > nul
-set /a x+=1
+rem Wait for pause seconds before looping (can be changed)
+timeout /t %pause% > nul
+set /a counter+=1
 goto loop
